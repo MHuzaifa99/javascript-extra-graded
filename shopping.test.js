c26322774da57a9afc08f6ced07f31012aec0fe9
Muhammad Huzaifa <muhammadhuzaifa457@gmail.com>
2023-04-10 22:53:29 +0500

    wrong test case for itemized list

---
+++ shopping.test.js
@@ -0,0 +1,54 @@
+const { myCart, addItemInCart, totalPrice, quantityUpdate } = require('./src/myCart')
+
+test('Cart should be empty', () => {
+    // Arrang
+    const assert = [];
+    // Act
+    const result = myCart();
+    // Assert
+    expect(result).toEqual(assert)
+})
+
+test('add an item in a cart', () => {
+    // Arrang
+    const obj = {
+        id: 1,
+        name: "handbags",
+        quantity: 2,
+        price: 500,
+    }
+    addItemInCart(obj)
+    const assert = 1000
+    // Act
+    const result = totalPrice();
+    // Assert
+    expect(result).toBe(assert)
+})
+
+test('already added item`s quantity change', () => {
+    // Arrang
+    const obj = {
+        id: 1,
+        quantity: 4
+    };
+    const assert = {
+        id: 1,
+        name: "handbags",
+        price: 500,
+        quantity: 4,
+        totalPrice: 1000
+    }
+    // Act
+    const result = quantityUpdate(obj)
+    // Assert
+    expect(result).toEqual(assert)
+})
+
+test("List of itemized of cart", () =>{
+    // Arrang
+    const assert = []
+    // Act
+    const result = myCart()
+    // Assert
+    expect(result).toBe(assert)
+})
---
+++ src/myCart.js
@@ -0,0 +1,26 @@
+var cartItem = [];
+exports.myCart = () => {
+    return cartItem
+}
+
+exports.addItemInCart = (obj) => {
+    obj.totalPrice = obj.quantity * obj.price
+    cartItem.push(obj)
+}
+
+exports.totalPrice = () => {
+    let totalPrice = 0;
+    for (let i of cartItem) {
+        totalPrice = totalPrice + i.totalPrice
+    }
+    return totalPrice;
+}
+
+exports.quantityUpdate = (obj) => {
+    return cartItem.map(item => {
+        if (item.id == obj.id) {
+            item.quantity = obj.quantity
+            return item
+        }
+    })[0]
+}
